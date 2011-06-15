8ccd6954b94a8c1c46cd54400ce0020e84fa8522
Esdras Mayrink <falecom@oesdras.com.br>
2011-06-15 05:43:47 -0300

    updated extension

--- ext/levenshtein_in_c/extconf.rb
+++ ext/levenshtein_in_c/extconf.rb
@@ -1,10 +1,10 @@
 require "mkmf"
 
-dir_config("levenshtein")
+dir_config("levenshtein_in_c")
 
 have_library("levenshtein_array")
 have_library("levenshtein_array_of_strings")
 have_library("levenshtein_generic")
 have_library("levenshtein_string")
 
-create_makefile("levenshtein/levenshtein_in_c")
+create_makefile("levenshtein_in_c/levenshtein_in_c")
